bb8721216c02fd10ac90b7bd4e6a8a075ebe0049
ZakPrimozic <primozic.zak@gmail.com>
2025-03-28 07:40:14 +0000

    oddano 28.03.2025 (html glavna stran)

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def glavna_stran():
+    return render_template("glavna.html")
+
+
+
+@app.route('/login')
+def login():
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
