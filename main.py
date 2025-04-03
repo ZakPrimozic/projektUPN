44a9330330e12b2bf90ac6cc94aacc6654d8955a
ZakPrimozic <primozic.zak@gmail.com>
2025-04-03 10:52:30 +0000

    popravek in dodajanje 1 letnika

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def glavna_stran():
     return render_template("glavna.html")
 
+@app.route('/1letnik')
+def letnik1():
+    return render_template("1letnik.html")
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
