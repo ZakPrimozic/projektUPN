4bb407c223be3bcf3aceef57c629a1cdc80448bc
ZakPrimozic <primozic.zak@gmail.com>
2025-04-04 06:16:41 +0000

    oddano vse datum 4.04 ( dodana baza za uporabnike, prijava)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+
+db = TinyDB("uporabni.json") 
+User = Query()
 
 @app.route('/')
 def glavna_stran():
@@ -10,5 +13,26 @@ def glavna_stran():
 @app.route('/1letnik')
 def letnik1():
     return render_template("1letnik.html")
+
+
+@app.route("/prijava")
+def prijava():
+    return render_template("prijava.html")
+
+
+@app.route("/shrani", methods=["POST"])
+def shrani_uporabnika():
+    nova_vnos = request.get_json()
+    
+    if not nova_vnos:
+        return jsonify({"error": "Manjkajo podatki!"}), 400
+
+    db.insert(nova_vnos)  
+    return jsonify({"message": "Podatki shranjeni!"}), 201
+
+@app.route("/uporabniki", methods=["GET"])
+def pridobi_uporabnike():
+    return jsonify(db.all())
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
